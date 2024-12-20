68ba2907743d324505dc79023c05c28c18833b79
Valentin-M.dev <valentin.mamalet@hotmail.fr>
2024-12-20 13:45:47 +0100

    Second Commit "CalculatriceC#"

--- CalculatriceC#/Program.cs
+++ CalculatriceC#/Program.cs
@@ -17,45 +17,122 @@ do
 
 
 
-
-    switch (menuChoix)
+    if (menuChoix <= 4)
     {
-        case 1:
-            {
-                bool stop = true;
-
-                do
+        switch (menuChoix)
+        {
+            case 1:
                 {
-                    Console.Clear();
-                    Console.WriteLine("\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
-                    Console.WriteLine("\u2551 Calculatrice C# \u2551");
-                    Console.WriteLine("\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D");
-                    Console.WriteLine("\n\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
-                    Console.WriteLine("\u2551 Veuillez entrer vos valeurs \u2551");
-                    Console.WriteLine("\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D");
-                    int valeurs = int.Parse(Console.ReadLine());
-                    mesValeurs.Add(valeurs);
-                    string confirmation;
-                    Console.WriteLine("\n\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550═\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
-                    Console.WriteLine("║Voulez vous continuer de saisir des valeurs║");
-                    confirmation = Console.ReadLine();
-                    if (confirmation == "oui" || confirmation == "o")
+                    bool stop = false;
+
+                    do
                     {
                         Console.Clear();
-                        
-                    }
-                    
-                    else
-                    {
-                        stop = false;
-                    }
-                }
-                while (stop == false);
-                Console.Clear();
-            }
-            break;
+                        Console.WriteLine("\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
+                        Console.WriteLine("\u2551 Calculatrice C# \u2551");
+                        Console.WriteLine("\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D");
+                        Console.WriteLine("\n\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
+                        Console.WriteLine("\u2551 Veuillez entrer vos valeurs \u2551");
+                        Console.WriteLine("\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D");
+                        int valeurs = int.Parse(Console.ReadLine());
 
+                        if (valeurs != 999)
+                        {
+
+                            mesValeurs.Add(valeurs);
+                        }
+                        else if (valeurs == 999)
+                        {
+                            stop = true;
+                        }
+                    }
+                    while (stop == false);
+                    Console.Clear();
+                }
+                break;
+
+            case 2:
+                {
+                    string operateur;
+                    int sommes;
+                    int valeursChoix1;
+                    int valeursChoix2;
+                    int result;
+
+                    Console.WriteLine("\n\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
+                    Console.WriteLine("\u2551 Veuillez choisir votre opérateur \u2551");
+                    Console.WriteLine("\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D");
+                    operateur = Console.ReadLine();
+                    Console.Write("Selectionner votre première valeur : ");
+                    valeursChoix1 = int.Parse(Console.ReadLine());
+                    Console.Write("Selectionner votre deuxième valeur : ");
+                    valeursChoix2 = int.Parse(Console.ReadLine());
+
+                    switch (operateur)
+                    {
+                        case "+":
+                            {
+                                sommes = mesValeurs[valeursChoix1] + mesValeurs[valeursChoix2];
+                                mesValeurs.Add(sommes);
+                                result = sommes;
+                            }
+                            break;
+
+                        case "-":
+                            {
+                                sommes = mesValeurs[valeursChoix1] - mesValeurs[valeursChoix2];
+                                mesValeurs.Add(sommes);
+                                result = sommes;
+                            }
+                            break;
+
+                        case "*":
+                            {
+                                sommes = mesValeurs[valeursChoix1] * mesValeurs[valeursChoix2];
+                                mesValeurs.Add(sommes);
+                                result = sommes;
+                            }
+                            break;
+
+                        case "/":
+                                sommes = mesValeurs[valeursChoix1] / mesValeurs[valeursChoix2];
+                                mesValeurs.Add(sommes);
+                                result = sommes;
+                            break;
+                    }
+
+                    Console.Clear();
+                }
+                break;
+
+            case 3:
+                {
+                    Console.Clear();
+                    int resultList = mesValeurs.LastOrDefault();
+
+                    Console.WriteLine("\n\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
+                    Console.WriteLine("\u2551\t\t" + resultList + "\t\t\u2551");
+                    Console.WriteLine("╚\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n");
+
+                }
+                break;
+
+            case 0:
+                {
+                    Environment.Exit(0);
+                }
+                break;
+        }
     }
-   
+    else
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("\n\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550═══\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
+        Console.WriteLine("║Veuillez selectonner un chiffre entre 0 & 4║");
+        Console.WriteLine("╚\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550═══\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n");
+        Console.ResetColor();
+    }
+
 }
-while (menu) ;
+while (menu);
